edb1a5cf5fa4953f2b6583d269a5f9e0cd983f2c
Mahmoud Qandeel <99659529+qandeeil@users.noreply.github.com>
2024-06-09 22:42:24 +0300

    add ISignup and ILogin interface

--- src/components/registration/signup/Interfaces.ts
+++ src/components/registration/signup/Interfaces.ts
@@ -3,3 +3,17 @@ export interface InfoAccountTypeCard {
   title: string;
   description: string;
 }
+
+export interface ISignup {
+  token: string;
+  name: string;
+  email: string;
+  phone_number: string | undefined;
+  password: string;
+}
+
+export interface ILogin {
+  identity: string;
+  password: string;
+  country: string;
+}
