45c2c12199cb7696941d0e4fdda24fdffcbe1e9f
Mahmoud Qandeel <99659529+qandeeil@users.noreply.github.com>
2024-06-09 22:37:28 +0300

    login config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,31 +6,62 @@ import "@/styles/registration/login/login.scss";
 import HeaderForm from "@/components/registration/sharing/HeaderForm";
 import CustomInput from "@/components/registration/sharing/CustomInput";
 import CustomButton from "@/components/registration/sharing/CustomButton";
-import { useState } from "react";
+import { useLayoutEffect, useState } from "react";
+import { useAppDispatch } from "@/store/hooks";
+import { loginThunk } from "@/store/registration/user";
+import { ILogin } from "@/components/registration/signup/Interfaces";
+import axios from "axios";
 
 interface InfoUser {
-  email: string;
+  identity: string;
   password: string;
 }
 
 export default function Home() {
-  const [infoUser, setInfoUser] = useState<InfoUser>({
-    email: "",
+  const dispatch = useAppDispatch();
+  const [infoUser, setInfoUser] = useState<ILogin>({
+    identity: "",
     password: "",
+    country: "",
   });
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isError, setIsError] = useState<any>();
+  const [country, setCounry] = useState<string>("");
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
     setInfoUser((prevState: InfoUser) => ({
       ...prevState,
       [name]: value,
+      country: country,
     }));
   };
 
-  const loginHandler = (event: any) => {
-    event.preventDefault();
-    setIsLoading(!isLoading)
+  useLayoutEffect(() => {
+    const getCodeCountry = async () => {
+      const countryResponse = await axios.get("https://api.country.is");
+      const country = countryResponse.data.country;
+      setCounry(country);
+    };
+    getCodeCountry();
+  }, []);
+
+  const loginHandler = async () => {
+    setIsLoading(true);
+    setIsError(null);
+
+    try {
+      const loginResponse: any = await dispatch(loginThunk(infoUser));
+      if (loginResponse.error) {
+        setIsError(loginResponse.payload);
+      } else {
+        console.log(">> loginHandler ", loginResponse);
+      }
+    } catch (error: any) {
+      setIsError(error.message);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
@@ -41,7 +72,12 @@ export default function Home() {
           <span className="go-navigation">
             Don't have an account? <Link href={"/registration"}>Sign Up</Link>
           </span>
-          <form onSubmit={(e) => loginHandler(e)}>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              loginHandler();
+            }}
+          >
             <HeaderForm
               title={"Account Login"}
               description={
@@ -49,12 +85,14 @@ export default function Home() {
               }
             />
             <CustomInput
-              lable={"Email address or phone number or username"}
-              placeholder={"Email address or phone number or username"}
-              type={"email"}
+              lable={"Email address or phone number"}
+              placeholder={"Email address or phone number"}
+              type={"text"}
               onChange={handleInputChange}
-              value={infoUser.email}
-              name="email"
+              value={infoUser.identity}
+              name="identity"
+              isError={isError?.email === false}
+              message={isError?.message}
             />
             <CustomInput
               lable={"Password"}
@@ -63,6 +101,8 @@ export default function Home() {
               onChange={handleInputChange}
               value={infoUser.password}
               name="password"
+              isError={isError?.password === false}
+              message={isError?.message}
             />
             <CustomButton
               onClick={loginHandler}
