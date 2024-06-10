3013b2a969720dbc752507dc3fe105fb29e9f01c
Mahmoud Qandeel <99659529+qandeeil@users.noreply.github.com>
2024-06-10 19:19:10 +0300

    create component fillter events

---
+++ src/components/sharing/FilterEvents.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "@/styles/sharing/filterEvents.scss";
+
+type Props = {};
+
+const FilterEvents = (props: Props) => {
+  return (
+    <div className="filterEvents">
+      <h1 className="title">Filter Events</h1>
+    </div>
+  );
+};
+
+export default FilterEvents;
