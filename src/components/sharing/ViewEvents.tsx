59e32672c8a5aff76e3665e3bfb6e8b25f3356f5
Mahmoud Qandeel <99659529+qandeeil@users.noreply.github.com>
2024-06-10 19:19:36 +0300

    create component view events

---
+++ src/components/sharing/ViewEvents.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '@/styles/sharing/viewEvents.scss'
+
+type Props = {}
+
+const ViewEvents = (props: Props) => {
+  return (
+    <div className='viewEvents'>viewEvents</div>
+  )
+}
+
+export default ViewEvents
