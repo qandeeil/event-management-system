8e95326af850aa5cccb459a5f5424bf048ba86f0
Mahmoud Qandeel <99659529+qandeeil@users.noreply.github.com>
2024-06-10 19:20:15 +0300

    add header and filter events and view events

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,20 +2,22 @@
 import React from "react";
 import { signOut, useSession, getSession } from "next-auth/react";
 import { SessionProvider } from "next-auth/react";
+import Header from "@/components/sharing/Header";
+import "@/styles/screens/homeScreen.scss";
+import { getServerSession } from "next-auth";
+import FilterEvents from "@/components/sharing/FilterEvents";
+import ViewEvents from "@/components/sharing/ViewEvents";
 
 type Props = {};
 
 const HomeScreen = (props: Props) => {
   return (
-    <div>
-      <button
-        onClick={(e) => {
-          e.preventDefault();
-          signOut();
-        }}
-      >
-        logout
-      </button>
+    <div className="home_screen">
+      <Header />
+      <div className="container">
+        <FilterEvents />
+        <ViewEvents />
+      </div>
     </div>
   );
 };
