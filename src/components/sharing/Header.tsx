ff3ecff6be42f6ef5a72db28575c4e6ed90e40c1
Mahmoud Qandeel <99659529+qandeeil@users.noreply.github.com>
2024-06-12 18:47:35 +0300

    add Setting sDialog

--- src/components/sharing/Header.tsx
+++ src/components/sharing/Header.tsx
@@ -7,6 +7,7 @@ import phoneSVG from "../../../public/icon/phone.svg";
 import starSVG from "../../../public/icon/star.svg";
 import "@/styles/sharing/header.scss";
 import { getSession, signOut } from "next-auth/react";
+import SettingsDialog from "./SettingsDialog";
 
 type Props = {};
 
@@ -33,48 +34,57 @@ const Header = (props: Props) => {
 
     return firstInitial + secondInitial;
   };
+  const [openSettingsDialog, setOpenSettingsDialog] = React.useState(false);
   return (
-    <div className="main_header">
-      <div className="contact">
-        <Image src={logo} alt="logo" width={147} height={33} />
-        <div className="info">
-          <div className="phone infoContact">
-            <Image src={phoneSVG} alt="phone" />
-            <span>+1 (678) 999 3323</span>
-          </div>
-          <div className="email infoContact">
-            <Image src={emailSVG} alt="email" />
-            <span>info@EventTitans.com</span>
-          </div>
-        </div>
-      </div>
-      <div className="container">
-        <div className="my_account" onClick={() => setShowMenu(!showMenu)}>
-          <span className="profile_image">
-            {user?.profile_image ? (
-              <Image src={starSVG} alt="star" />
-            ) : (
-              getInitials(user?.name)
-            )}
-          </span>
-          <span className="profile_name">{user?.name?.slice(0, 15)}</span>
-          <div className={`box-menu ${showMenu ? "open" : ""}`}>
-            <ul>
-              <li>My Favorites</li>
-              <li>My Reservations</li>
-              <li>Settings</li>
-              <li onClick={() => signOut()}>Sign out</li>
-            </ul>
+    <>
+      <div className="main_header">
+        <div className="contact">
+          <Image src={logo} alt="logo" width={147} height={33} />
+          <div className="info">
+            <div className="phone infoContact">
+              <Image src={phoneSVG} alt="phone" />
+              <span>+1 (678) 999 3323</span>
+            </div>
+            <div className="email infoContact">
+              <Image src={emailSVG} alt="email" />
+              <span>info@EventTitans.com</span>
+            </div>
           </div>
         </div>
-        {user?.admin && (
-          <div className="newEvent">
-            <Image src={starSVG} alt="starSVG" />
-            <span>Create Event</span>
+        <div className="container">
+          <div className="my_account" onClick={() => setShowMenu(!showMenu)}>
+            <span className="profile_image">
+              {user?.profile_image ? (
+                <Image src={starSVG} alt="star" />
+              ) : (
+                getInitials(user?.name)
+              )}
+            </span>
+            <span className="profile_name">{user?.name?.slice(0, 15)}</span>
+            <div className={`box-menu ${showMenu ? "open" : ""}`}>
+              <ul>
+                <li>My Favorites</li>
+                <li>My Reservations</li>
+                <li onClick={() => setOpenSettingsDialog(true)}>Settings</li>
+                <li onClick={() => signOut()}>Sign out</li>
+              </ul>
+            </div>
           </div>
-        )}
+          {user?.admin && (
+            <div className="newEvent">
+              <Image src={starSVG} alt="starSVG" />
+              <span>Create Event</span>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+      <SettingsDialog
+        openSettingsDialog={openSettingsDialog}
+        setOpenSettingsDialog={(e: boolean) => setOpenSettingsDialog(e)}
+        user={user}
+        getInitials={getInitials}
+      />
+    </>
   );
 };
 
