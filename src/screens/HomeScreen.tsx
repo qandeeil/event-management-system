aef1e5305f0e7938a669bf2c24ad1c3e80f437ba
Mahmoud Qandeel <99659529+qandeeil@users.noreply.github.com>
2024-06-12 18:48:31 +0300

    remove getServerSession

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,7 @@
 "use client";
 import React from "react";
-import { signOut, useSession, getSession } from "next-auth/react";
-import { SessionProvider } from "next-auth/react";
 import Header from "@/components/sharing/Header";
 import "@/styles/screens/homeScreen.scss";
-import { getServerSession } from "next-auth";
 import FilterEvents from "@/components/sharing/FilterEvents";
 import ViewEvents from "@/components/sharing/ViewEvents";
 
