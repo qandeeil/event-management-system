90274d2a72c5aae14b1610c2e160876e4460279b
Mahmoud Qandeel <99659529+qandeeil@users.noreply.github.com>
2024-06-12 18:45:56 +0300

    check session expires

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,31 @@
+"use client";
 import "@/styles/registration/login/login.scss";
 import HomeScreen from "@/screens/HomeScreen";
+import { getSession, signOut } from "next-auth/react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [shouldSignOut, setShouldSignOut] = useState(false);
+
+  useEffect(() => {
+    const checkSession = async () => {
+      const session = await getSession();
+      if (session && session.expires) {
+        const expirationTime = new Date(session.expires).getTime();
+        const currentTime = Date.now();
+        if (currentTime > expirationTime) {
+          setShouldSignOut(true);
+        }
+      }
+    };
+
+    checkSession();
+  }, []);
+
+  useEffect(() => {
+    if (shouldSignOut) {
+      signOut();
+    }
+  }, [shouldSignOut]);
   return <HomeScreen />;
 }
