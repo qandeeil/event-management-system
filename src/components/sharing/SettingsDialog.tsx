e3e8f2285b8497d2e8573be17415ee5052375e51
Mahmoud Qandeel <99659529+qandeeil@users.noreply.github.com>
2024-06-12 18:47:58 +0300

    build SettingsDialog

---
+++ src/components/sharing/SettingsDialog.tsx
@@ -0,0 +1,208 @@
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  styled,
+  TextField,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import "@/styles/sharing/settingsDialog.scss";
+import Image from "next/image";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import CustomInput from "../registration/sharing/CustomInput";
+import { IEditAccount } from "../registration/signup/Interfaces";
+import CustomInputPhoneNumber from "../registration/sharing/CustomInputPhoneNumber";
+import { useAppDispatch } from "@/store/hooks";
+import { updateAccountThunk } from "@/store/registration/user";
+
+type Props = {
+  openSettingsDialog: boolean;
+  setOpenSettingsDialog: (v: boolean) => void;
+  user: any;
+  getInitials: any;
+};
+
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
+  height: 1,
+  overflow: "hidden",
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  whiteSpace: "nowrap",
+  width: 1,
+});
+
+const SettingsDialog = ({
+  openSettingsDialog,
+  setOpenSettingsDialog,
+  user,
+  getInitials,
+}: Props) => {
+  const dispatch = useAppDispatch();
+  const handleClose = () => {
+    setOpenSettingsDialog(false);
+  };
+  const [selectImage, setSelectImage] = useState();
+  const [imageUrl, setImageUrl] = useState<any>(null);
+  const handleFileChange = (event: any) => {
+    const file = event.target.files[0];
+    if (file) {
+      setSelectImage(file);
+      setImageUrl(URL.createObjectURL(file));
+    }
+  };
+  const [userData, setUserData] = useState<IEditAccount>({
+    token: "token",
+    name: user?.name,
+    email: user?.email,
+    phone_number: user?.phone_number,
+    profile_image: selectImage,
+  });
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setUserData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  useEffect(() => {
+    setUserData({
+      token: "",
+      name: user?.name,
+      email: user?.email,
+      phone_number: user?.phone_number,
+      profile_image: user?.profile_image,
+    });
+  }, [user]);
+
+  useEffect(() => {
+    setUserData((prevState) => ({
+      ...prevState,
+      profile_image: selectImage,
+    }));
+  }, [selectImage]);
+
+  const updateDataHandler = async () => {
+    const response = await dispatch(updateAccountThunk(userData));
+    console.log(">> response: ", response);
+  };
+
+  return (
+    <Dialog
+      open={openSettingsDialog}
+      onClose={handleClose}
+      className="settingsDialog individual-screen"
+      maxWidth="lg"
+    >
+      <DialogTitle sx={{ fontWeight: "bold" }}>Account settings</DialogTitle>
+      <DialogContent
+        className="dialogContent"
+        sx={{ display: "flex", flex: 1 }}
+      >
+        <Box
+          noValidate
+          component="form"
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            m: "auto",
+            width: "fit-content",
+            gap: 3,
+          }}
+        >
+          <div className="profileImage">
+            <label htmlFor="uploadImage" className="containerImage">
+              {user?.profile_image || imageUrl ? (
+                <Image
+                  src={imageUrl || user?.profile_image}
+                  alt={user?.name}
+                  width={900}
+                  height={400}
+                  className="profile_image"
+                />
+              ) : (
+                getInitials(user?.name)
+              )}
+            </label>
+            <Button
+              component="label"
+              role={undefined}
+              variant="contained"
+              tabIndex={-1}
+              startIcon={<CloudUploadIcon />}
+              style={{ width: "100%", height: "3rem" }}
+            >
+              Upload photo
+              <VisuallyHiddenInput
+                type="file"
+                accept="image/*"
+                onChange={handleFileChange}
+                id="uploadImage"
+              />
+            </Button>
+          </div>
+          <form className="form">
+            <CustomInput
+              lable={"Full name"}
+              placeholder={"Full name"}
+              type={"text"}
+              onChange={handleInputChange}
+              value={userData.name}
+              name="name"
+              isError={user?.isError?.name === false}
+              message={user?.isError?.message}
+            />
+            <CustomInput
+              lable={"Email Address"}
+              placeholder={"Email Address"}
+              type={"email"}
+              onChange={handleInputChange}
+              value={userData.email}
+              name="email"
+              isError={user?.isError?.email === false}
+              message={user?.isError?.message}
+            />
+            <CustomInputPhoneNumber
+              lable="Phone Number"
+              onChange={(e: string | undefined) =>
+                setUserData((prevData) => ({
+                  ...prevData,
+                  phone_number: e,
+                }))
+              }
+              value={userData.phone_number}
+              isError={user?.isError?.phone_number === false}
+              message={user?.isError?.message}
+            />
+            <CustomInput
+              lable={"Account Type"}
+              placeholder={"Account Type"}
+              type={"text"}
+              onChange={handleInputChange}
+              value={user?.account_type}
+              name="account_type"
+              isError={user?.isError?.account_type === false}
+              message={user?.isError?.message}
+            />
+          </form>
+        </Box>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose} color="error" sx={{ fontWeight: "bold" }}>
+          Close
+        </Button>
+        <Button onClick={updateDataHandler} sx={{ fontWeight: "bold" }}>
+          Save
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default SettingsDialog;
