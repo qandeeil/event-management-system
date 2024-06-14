d4317093e34d472d10296988d67e0db6412fdde4
Mahmoud Qandeel <99659529+qandeeil@users.noreply.github.com>
2024-06-14 16:02:46 +0300

    add checkSession

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -4,18 +4,37 @@ import Header from "@/components/sharing/Header";
 import "@/styles/screens/homeScreen.scss";
 import FilterEvents from "@/components/sharing/FilterEvents";
 import ViewEvents from "@/components/sharing/ViewEvents";
+import { getSession, SessionProvider, signOut } from "next-auth/react";
 
-type Props = {};
+type Props = {
+  token: string | undefined;
+};
 
-const HomeScreen = (props: Props) => {
+const HomeScreen = ({ token }: Props) => {
+  const checkSession = async () => {
+    const session = await getSession();
+    console.log(session);
+    if (session && session.expires) {
+      const expirationTime = new Date(session.expires).getTime();
+      const currentTime = Date.now();
+      console.log(">> currentTime: ", currentTime);
+      console.log(">> expirationTime: ", expirationTime);
+      if (currentTime > expirationTime) {
+        signOut();
+      }
+    }
+  };
+  checkSession();
   return (
-    <div className="home_screen">
-      <Header />
-      <div className="container">
-        <FilterEvents />
-        <ViewEvents />
+    <SessionProvider>
+      <div className="home_screen">
+        <Header token={token} />
+        <div className="container">
+          <FilterEvents />
+          <ViewEvents />
+        </div>
       </div>
-    </div>
+    </SessionProvider>
   );
 };
 
