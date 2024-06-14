db4928ab53fdd11e61e3497173a7ee78843edafe
Mahmoud Qandeel <99659529+qandeeil@users.noreply.github.com>
2024-06-14 16:01:40 +0300

    remove checkSession

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,10 @@
-"use client";
 import "@/styles/registration/login/login.scss";
 import "@/styles/screens/homeScreen.scss";
 import HomeScreen from "@/screens/HomeScreen";
-import { getSession, signOut } from "next-auth/react";
-import { useEffect, useState, Suspense } from "react";
-import { useSearchParams } from "next/navigation";
-
-function HomeComponent() {
-  const searchParams = useSearchParams();
-  const [shouldSignOut, setShouldSignOut] = useState(false);
-
-  useEffect(() => {
-    const checkSession = async () => {
-      const session = await getSession();
-      if (session && session.expires) {
-        const expirationTime = new Date(session.expires).getTime();
-        const currentTime = Date.now();
-        if (currentTime > expirationTime) {
-          setShouldSignOut(true);
-        }
-      }
-    };
-
-    checkSession();
-  }, []);
-
-  useEffect(() => {
-    if (shouldSignOut) {
-      signOut();
-    }
-  }, [shouldSignOut]);
-
-  return <HomeScreen />;
-}
+import { cookies } from "next/headers";
 
 export default function Home() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <HomeComponent />
-    </Suspense>
-  );
+  const cookieStore = cookies();
+  const accessToken = cookieStore.get("accessToken");
+  return <HomeScreen token={accessToken?.value} />;
 }
