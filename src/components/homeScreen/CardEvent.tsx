a79d00ad43a3b4c126423eed8abf422d70351730
Mahmoud Qandeel <99659529+qandeeil@users.noreply.github.com>
2024-06-16 23:17:32 +0300

    build CardEvent

---
+++ src/components/homeScreen/CardEvent.tsx
@@ -0,0 +1,84 @@
+import React, { forwardRef } from "react";
+import "@/styles/screens/homeScreen/cardEvent.scss";
+import Image from "next/image";
+import bookmarkSVG from "../../../public/icon/bookmark.svg";
+import calendarSVG from "../../../public/icon/calendar.svg";
+import locationSVG from "../../../public/icon/location.svg";
+import LINK_BACKEND from "@/store/baseURL";
+
+type Props = {
+  event: any;
+};
+
+const CardEvent = forwardRef<HTMLDivElement, Props>(({ event }, ref) => {
+  function formatDate(dateString: any) {
+    const date = new Date(dateString);
+    const options: any = { day: "numeric", month: "short", year: "numeric" };
+    return date.toLocaleDateString("en-GB", options).replace(/ /g, " ");
+  }
+
+  return (
+    <div className="card-event" ref={ref}>
+      <div className="container-image">
+        <div className="image">
+          <Image
+            src={LINK_BACKEND + "/" + event?.cover_photo}
+            alt="image"
+            width={200}
+            height={200}
+          />
+        </div>
+        <div className="container-top">
+          <div className="price">
+            <span>$ {event?.price}</span>
+          </div>
+          <div className="save">
+            <Image src={bookmarkSVG} alt="bookmark" width={50} height={50} />
+          </div>
+        </div>
+        <div className="container-bottom">
+          <div className="paid">
+            <span>Paid</span>
+          </div>
+        </div>
+      </div>
+      <div className="info-event">
+        <div className="by">
+          <span>By</span>
+          <div className="info-created">
+            <Image
+              src={LINK_BACKEND + "/" + event?.creator?.profile_image}
+              alt="image"
+              width={25}
+              height={25}
+            />
+            <span>{event?.creator.name}</span>
+          </div>
+        </div>
+        <h1 className="title">{event?.title}</h1>
+        <div className="date">
+          <Image src={calendarSVG} alt="calendar" width={23} height={23} />
+          <div className="from da">
+            <span>From</span>
+            <span>{formatDate(event?.date.start_date)}</span>
+          </div>
+          <div className="to da">
+            <span>To</span>
+            <span>{formatDate(event?.date.end_date)}</span>
+          </div>
+        </div>
+        <div className="location date">
+          <Image src={locationSVG} alt="location" width={23} height={23} />
+          <div className="from da">
+            <span>Location</span>
+            <span>
+              {event?.location.city}, {event?.location.country}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+});
+
+export default CardEvent;
