a41f1ac8325e846379db06e2018fe10c8e768b63
Mahmoud Qandeel <99659529+qandeeil@users.noreply.github.com>
2024-06-16 23:17:22 +0300

    build view event

--- src/components/sharing/ViewEvents.tsx
+++ src/components/sharing/ViewEvents.tsx
@@ -1,12 +1,63 @@
-import React from 'react'
-import '@/styles/sharing/viewEvents.scss'
+import React, { useEffect, useRef } from "react";
+import "@/styles/sharing/viewEvents.scss";
+import HeaderViewEvent from "../homeScreen/HeaderViewEvent";
+import CardEvent from "../homeScreen/CardEvent";
+import Loading from "./Loading";
 
-type Props = {}
+type Props = {
+  events: any;
+  setPage: any;
+  fetchData: boolean;
+  isLoading: boolean;
+};
 
-const ViewEvents = (props: Props) => {
+const ViewEvents = ({ events, setPage, fetchData, isLoading }: Props) => {
+  const observerRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const lastElement = entries[0];
+        if (lastElement.isIntersecting && fetchData) {
+          setPage((prevValue: any) => prevValue + 1);
+        }
+      },
+      {
+        threshold: 0.5,
+      }
+    );
+
+    if (observerRef.current) {
+      observer.observe(observerRef.current);
+    }
+
+    return () => {
+      if (observerRef.current) {
+        observer.unobserve(observerRef.current);
+      }
+    };
+  }, [events]);
+
   return (
-    <div className='viewEvents'>viewEvents</div>
-  )
-}
+    <div className="viewEvents">
+      <HeaderViewEvent />
+      <div className="container-card-events">
+        {events.map((item: any, index: number) => (
+          <CardEvent
+            key={item._id}
+            event={item}
+            ref={index === events.length - 1 ? observerRef : null}
+          />
+        ))}
 
-export default ViewEvents
+        {isLoading ? (
+          <div className="loading-content">
+            <div className="loader"></div>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
+export default ViewEvents;
