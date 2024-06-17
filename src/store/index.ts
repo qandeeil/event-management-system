e8512254966f31282ab602192348cd129c5cdcce
Mahmoud Qandeel <99659529+qandeeil@users.noreply.github.com>
2024-06-18 00:22:20 +0300

    add favoritesSlice

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "@/store/user/user";
 import tokenSlice from "@/store/user/token";
 import eventSlice from "@/store/event/event";
+import favoritesSlice from "@/store/favorites/favorites";
 
 export const store = configureStore({
   reducer: {
     userSlice,
     tokenSlice,
     eventSlice,
+    favoritesSlice,
   },
 });
 
