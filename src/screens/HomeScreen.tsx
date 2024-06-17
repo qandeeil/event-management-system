76665de9d728c6765b335f359e326cc7a2374a97
Mahmoud Qandeel <99659529+qandeeil@users.noreply.github.com>
2024-06-17 13:21:45 +0300

    remove checkSession and SessionProvider

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,38 +1,29 @@
 "use client";
-import React, { useEffect, useState, useCallback, useRef } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import Header from "@/components/sharing/Header";
 import "@/styles/screens/homeScreen/homeScreen.scss";
 import FilterEvents from "@/components/sharing/FilterEvents";
 import ViewEvents from "@/components/sharing/ViewEvents";
-import { getSession, SessionProvider, signOut } from "next-auth/react";
+import { getSession } from "next-auth/react";
 import { useAppDispatch } from "@/store/hooks";
 import { getEventsThunk } from "@/store/event/event";
 
-type Props = {
-  token: string | undefined;
-};
+type Props = {};
 
-const HomeScreen = ({ token }: Props) => {
+const HomeScreen = ({}: Props) => {
   const [page, setPage] = useState<number>(1);
   const [events, setEvents] = useState<any[]>([]);
   const [fetchData, setFetchData] = useState<boolean>(true);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const dispatch = useAppDispatch();
-
-  // const checkSession = useCallback(async () => {
-  //   const session = await getSession();
-  //   if (session && session.expires) {
-  //     const expirationTime = new Date(session.expires).getTime();
-  //     const currentTime = Date.now();
-  //     if (currentTime > expirationTime) {
-  //       signOut();
-  //     }
-  //   }
-  // }, []);
-
-  // useEffect(() => {
-  //   checkSession();
-  // }, [checkSession]);
+  const [token, setToken] = useState<string>("");
+  const getSessionToken = async () => {
+    const session: any = await getSession();
+    setToken(session?.user.token);
+  };
+  useEffect(() => {
+    getSessionToken();
+  }, []);
 
   const getEventsHandler = useCallback(async () => {
     setIsLoading(true);
@@ -47,27 +38,25 @@ const HomeScreen = ({ token }: Props) => {
       }
       setIsLoading(false);
     }
-  }, [page]);
+  }, [page, token]);
 
   useEffect(() => {
-    fetchData && !isLoading && getEventsHandler();
-  }, [page]);
+    token && fetchData && !isLoading && getEventsHandler();
+  }, [page, token]);
 
   return (
-    <SessionProvider>
-      <div className="home_screen">
-        <Header token={token} />
-        <div className="container">
-          <FilterEvents />
-          <ViewEvents
-            events={events}
-            setPage={setPage}
-            fetchData={fetchData}
-            isLoading={isLoading}
-          />
-        </div>
+    <div className="home_screen">
+      <Header token={token} />
+      <div className="container">
+        <FilterEvents />
+        <ViewEvents
+          events={events}
+          setPage={setPage}
+          fetchData={fetchData}
+          isLoading={isLoading}
+        />
       </div>
-    </SessionProvider>
+    </div>
   );
 };
 
