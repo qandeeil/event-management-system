5012315ef3eacc90f192f92aced067d3c638e3db
Mahmoud Qandeel <99659529+qandeeil@users.noreply.github.com>
2024-06-24 15:17:34 +0300

    build my-favorites page

---
+++ src/app/my-favorites/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+import MySharingScreen from "@/screens/MySharingScreen";
+import { getFavoritesEvent } from "@/store/event/event";
+import { useAppDispatch } from "@/store/hooks";
+import { getSession } from "next-auth/react";
+import React, { useEffect, useState } from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  const dispatch = useAppDispatch();
+  const [token, setToken] = useState<string>("");
+  const [events, setEvents] = useState([]);
+  const [isLoading, setSsLoading] = useState<boolean>(true);
+  useEffect(() => {
+    getSessionToken();
+  }, []);
+
+  const getSessionToken = async () => {
+    const session: any = await getSession();
+    setToken(session?.user.token);
+  };
+  const getData = async () => {
+    setSsLoading(true);
+    await dispatch(getFavoritesEvent(token)).then((response) => {
+      if (typeof response.payload?.length) {
+        setEvents(response.payload);
+      } else {
+        setEvents([]);
+      }
+      setSsLoading(false);
+    });
+  };
+  useEffect(() => {
+    if (token) getData();
+  }, [token]);
+  return (
+    <MySharingScreen token={token} events={events} isLoading={isLoading} />
+  );
+};
+
+export default page;
